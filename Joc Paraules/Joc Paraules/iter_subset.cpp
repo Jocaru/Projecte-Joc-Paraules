d3d4a4586a5ca1c0fc792f760dd618853e5930a7
Jocaru <lgray.foxl@gmail.com>
2012-12-12 22:31:22 +0100

    Update Joc Paraules/Joc Paraules/iter_subset.cpp

--- Joc Paraules/Joc Paraules/iter_subset.cpp
+++ Joc Paraules/Joc Paraules/iter_subset.cpp
@@ -1,5 +1,13 @@
 iter_subset::iter_subset(nat n, nat k) throw(error){
-    
+    if (n >= k) {
+        nat aux1 = 1;
+        for (nat i = 0; i < (n - k); i++) aux1 = aux1 * n - i;
+        nat aux2 = 1;
+        for (nat i = k; i > 0; i--) aux2 = aux2 * i;
+        nat tmn = aux1 / aux2;
+        _v.resize(tmn);                                     //redimensionem el vector amb el nombre de subconjunts
+        for (nat i = 0; i < tmn; i++) _v[i].resize(k);      //redimensionem cada subconjunt a k
+    }
 }
 
 iter_subset::iter_subset(const iter_subset& its) throw(error){
